d86a842aef7c2a65ec7a177a9e1b6c46b76c0358
Alessandro Franceschi <al@lab42.it>
2012-01-21 13:08:09 +0100

    Added check-module.sh script

---
+++ check-module.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ ! -f manifests/init.pp ] ; then
+    echo "I don't find manifests/init.pp "
+    echo "Run this script from a module base directory"
+    exit 1
+fi
+
+echo "############################"
+echo "### Executing rake tasks ###"
+echo "############################"
+rake
+
+echo "############################"
+echo "### Executing puppet doc ###"
+echo "############################"
+for file in $( find . | grep ".pp$" ) ; do
+    echo "### $file"
+    puppet doc $file
+done
+
+echo "############################"
+echo "### Executing puppetlint ###"
+echo "############################"
+for file in $( find . | grep ".pp$" ) ; do
+    echo "### $file"
+    puppet-lint $file
+done
+
