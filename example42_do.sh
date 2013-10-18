83754fc6e5c0054c2d4633193f7e332fbd913b06
Alessandro Franceschi <al@lab42.it>
2013-10-18 12:12:25 +0200

    Adapting scripts for multi branch management

---
+++ example42_do.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Do things on example42 only modules
+if [ -f example42only_list.txt ] ; then 
+  echo "Found example42only_list.txt"
+else
+  echo "example42only_list.txt not found. Exiting."
+  exit 1
+fi
+
+COMMAND=${*:-"echo Provide a command to execute on all the modules in example42only_list.txt"}
+MODULELIST=$(cat example42only_list.txt)
+
+for a in $MODULELIST; do
+  cd $a
+  $COMMAND
+  cd ..
+done
--- sync.sh
+++ sync.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+collection=$(basename `pwd`)
+case "$collection" in
+  puppet-modules-nextgen) $suggestedbranch='2.x' ;;
+  puppet-modules-stdmod) $suggestedbranch='3.x' ;;
+  puppet-modules) $suggestedbranch='master' ;;
+  *) $suggestedbranch='master' ;;
+esac
+
+branch={$1:-$suggestedbranch}
+
 git pull origin master
 git submodule sync
 git submodule init
 git submodule update
-git submodule foreach git checkout master
-git submodule foreach git pull origin master
+git submodule foreach git checkout $branch
+git submodule foreach git pull origin $branch
