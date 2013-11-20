181f91d3b036f3596d3ad8f71892ba38dc755e3d
Alessandro Franceschi <al@lab42.it>
2013-11-20 11:49:43 +0100

    Mac support on massfindandreplace_macspurious.sh

--- massfindandreplace_macspurious.sh
+++ massfindandreplace_macspurious.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-OLDPROJECT=" "
-NEWPROJECT=" "
+OLDSTRING=" "
+NEWSTRING=" "
 
-for file in $( grep -R "$OLDPROJECT" . | grep -v ".git" | cut -d ":" -f 1 ) ; do
-        sed -i "s/$OLDPROJECT/$NEWPROJECT/g" $file && echo "Changed $file" # Use under Linux
+for file in $( grep -R "$OLDSTRING" . | grep -v ".git" | cut -d ":" -f 1 ) ; do
+    # Detect OS
+    if [ -f /mach_kernel ] ; then
+      sed -i "" -e "s/$OLDSTRING/$NEWSTRING/g" $file && echo "Changed $file"
+    else
+      sed -i "s/$OLDSTRING/$NEWSTRING/g" $file && echo "Changed $file"
+    fi
 done
